1ccc1d4e4ee2ec23461dad8c78d7927e0b8b8a16
宇 王 <wy_yxzx@163.com>
2019-07-17 22:12:39 +0800

    O(n^2)

--- Maximum Subarray/Maximum Subarray/Maximum Subarray.cpp
+++ Maximum Subarray/Maximum Subarray/Maximum Subarray.cpp
@@ -8,13 +8,27 @@ using namespace std;
 class Solution {
 public:
 	int maxSubArray(vector<int>& nums) {
-		
+		int size = nums.size();
+		if (size == 1) { return nums[0]; }
+		int max = INT_FAST32_MIN;
+		int temp = INT_FAST32_MIN;
+		for (int i = 0; i < size; i++)
+		{
+			temp = nums[i];
+			if (temp > max) { max = temp; }
+			for (int j = i+1; j < size; j++)
+			{
+				temp += nums[j];
+				if (temp > max) { max = temp; }
+			}
+		}
+		return max;
 	}
 };
 
 int main()
 {
-	vector<int> nums = { -2,1,-3,4,-1,2,1,-5,4 };
+	vector<int> nums = { -2,-1};
 	Solution test;
 	cout << test.maxSubArray(nums) << endl;
 }
