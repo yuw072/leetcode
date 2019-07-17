40580dec3dde6fe81c9d3cd03a07421980ee1c2e
宇 王 <wy_yxzx@163.com>
2019-07-17 23:09:27 +0800

    finished

---
+++ Plus One/Plus One/Plus One.cpp
@@ -0,0 +1,35 @@
+﻿// Plus One.cpp : https://leetcode-cn.com/problems/plus-one/
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> plusOne(vector<int>& digits) {
+		int size = digits.size();
+		int i = 0;
+		do
+		{
+			if (digits[size - 1 - i] != 9) 
+			{
+				digits[size - 1 - i] += 1; return digits; 
+			}
+			digits[size - 1 - i] = 0;
+			i++;
+		} while (i < size);
+		digits[0] = 1;
+		digits.push_back(0);
+		return digits;
+	}
+};
+
+int main()
+{
+	vector<int> digits = { 1,2,3,4 };
+	Solution test;
+	vector<int> temp = test.plusOne(digits);
+	cout << temp[0]<< temp[1]<< temp[2]<<temp[3] << endl;
+}
+
