2d640e2324848ed4eeb03306dbae662ca9da0f52
宇 王 <wy_yxzx@163.com>
2019-07-17 22:32:07 +0800

    O(n)but not good

---
+++ Length of Last Word/Length of Last Word/Length of Last Word.cpp
@@ -0,0 +1,23 @@
+﻿// Length of Last Word.cpp : https://leetcode-cn.com/problems/length-of-last-word/
+//2019.7.17
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		int size = s.size();
+		if (s.find(' ') == -1) { return size; }
+		if (size - s.find_last_of(' ') - 1 == 0) { return lengthOfLastWord(s.substr(0, s.size() - 1)); }
+		return size - s.find_last_of(' ')-1;
+	}
+};
+
+int main()
+{
+	string s = "asaa ";
+	Solution test;
+	cout << test.lengthOfLastWord(s) << endl;
+}
