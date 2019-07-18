a41c46db44eb73862ab4fa7dc34041441381e8af
宇 王 <wy_yxzx@163.com>
2019-07-18 23:24:03 +0800

    O(n)

---
+++ Same Tree/Same Tree/Same Tree.cpp
@@ -0,0 +1,40 @@
+﻿// Same Tree.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		if (p == NULL && q == NULL) { return 1; }
+		if (p == NULL && q != NULL) { return 0; }
+		if (p != NULL && q == NULL) { return 0; }
+		if (p->left == NULL && q->left != NULL) { return 0; }
+		if (q->left == NULL && p->left != NULL) { return 0; }
+		if (p->right == NULL && q->right != NULL) { return 0; }
+		if (q->right == NULL && p->right != NULL) { return 0; }
+		if (p->val == q->val) 
+		{
+			return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+		}
+		return 0;
+	}
+};
+
+int main()
+{
+	TreeNode p(12);
+	TreeNode q(12);
+	p.right = new TreeNode(60);
+	q.right = new TreeNode(72);
+	Solution test;
+	cout << test.isSameTree(&p, &q) << endl;
+}
